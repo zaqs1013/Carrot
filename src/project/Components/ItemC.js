3643fe3198a0e277cae72fe40932cba30085d0c8
zaqs1013 <zaqs1013@gmail.com>
2025-02-05 14:18:39 +0900

    ui update

--- src/project/Components/ItemC.js
+++ src/project/Components/ItemC.js
@@ -1,23 +1,36 @@
 import React from "react";
-import "./css/itemC.css"
+import "./css/itemC.css";
+
 const ItemC = ({ selectedId, topics }) => {
   const selectedItem = topics.find(item => item.id === selectedId);
 
   return (
     <div className="information">
       {selectedItem ? (
-          <div className="title">
-
-          <img src={selectedItem.image} alt={selectedItem.name} className="itemCsize" />
-          
-          <h2>{selectedItem.name}</h2>
-          <p><strong>장소:</strong> {selectedItem.location}</p>
-          <p><strong>구매 가능 여부:</strong> {selectedItem.purchase}</p>
-          <p><strong>특이사항:</strong></p>
-          <textarea readOnly>
-           {selectedItem.note}
-          </textarea>
+        <div>
+          <div className="info-card">
+            <img src={selectedItem.image} alt={selectedItem.name} className="itemCsize" />
+            <h2>{selectedItem.name}</h2>
           </div>
+
+          <div className="note-container">
+            <div className="note-card">
+              <h3>특이사항</h3>
+              <p>{selectedItem.note}</p>
+            </div>
+
+            <div className="note-card">
+              <h3>장소</h3>
+              <p>{selectedItem.location}</p>
+            </div>
+
+            <div className="note-card">
+              <h3>구매 가능 여부</h3>
+              <p>임시</p>
+              <p>{selectedItem.purchase}</p>
+            </div>
+          </div>
+        </div>
       ) : (
         <p>선택된 아이템이 없습니다.</p>
       )}
